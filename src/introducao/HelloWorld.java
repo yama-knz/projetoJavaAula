4018feb8bc37cfc22d329d874a0052710845c2a9
yama-knz <igorkenzoyamamoto@gmail.com>
2026-04-17 10:42:36 -0300

    Introdução aula JAVA

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+
+	}
+
+}
+
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,20 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		String nome = "Kenzo";
+		int idade = 25;
+		float graus = 20.0f;
+		boolean alistado;
+		
+		final String TURMA = "Turma 84";
+		final int QUANTIDADE = 50;
+		final double AREA = 3.1415;
+		
+		System.out.println(nome);
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+	}
+
+}
